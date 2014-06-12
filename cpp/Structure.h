4f4cb88e63ec11c55e6a01de6e3b48779fbbc42e
Konstantin Usevich <konstantin.usevich@statmod.ru>
2014-06-12 18:43:41 +0200

    Started to modify structure.

--- cpp/Structure.h
+++ cpp/Structure.h
@@ -1,5 +1,9 @@
-/** Abstract class for problem structure specification.
- * This class includes information about matrix structure and weights.
+/** Abstract class for specification of matrix structure and weights.
+ * This class contains information about:
+ *  - matrix structure \f$\mathscr{S}: \mathbb{R}^{n_p} \to \mathbb{R}^{m \times n}\f$,  
+ *    (equation \f$(\mathscr{S})\f$) in  in \cite slra-efficient );
+ *  - weight matrix \f$\mathrm{W}: \mathbb{R}^{n_p \times n_p}\f$, 
+ *     (equation \f$(\|\cdot\|^2_{\mathrm{W}})\f$ in \cite slra-efficient ).
  */
 class Structure {
 public:
@@ -8,9 +12,13 @@ public:
   virtual size_t getM() const = 0;      ///< Returns \f$m\f$
   virtual size_t getN() const = 0;      ///< Returns \f$n\f$
   
-  /** Fills matrix from given parameter vector */
-  virtual void fillMatrixFromP( gsl_matrix* c, const gsl_vector* p )  = 0; 
-  /** Computes \f$ p \leftarrow \beta p + \alpha G^T(R) y\f$. 
+  /** Creates a transpose of structured matrix from the parameter vector. 
+   * \param[out] c  matrix \f$c \leftarrow \mathscr{S}^{\top}(p)\f$
+   * \param[in]  p   parameter vector \f$p\in\mathbb{R}^{n_p}\f$
+   */
+  virtual void fillMatrixFromP( gsl_matrix* c, const gsl_vector* p ) = 0; 
+  
+  /** Modifes a vector as \f$ p \leftarrow \beta p + \alpha G^{\top}(R) y\f$. 
    * The fixed blocks may be skipped. */
   virtual void multByGtUnweighted( gsl_vector* p, const gsl_matrix *R, 
                                    const gsl_vector *y, double alpha = -1,
