3ad7a3ca93fc4d54f3f292e7c0a579357213c314
Konstantin Usevich <konstantin.usevich@statmod.ru>
2015-10-27 17:29:51 +0100

    Corrected Phi matrix in gcd_nls_complex.

--- agcd/gcd_nls_complex.m
+++ agcd/gcd_nls_complex.m
@@ -18,14 +18,13 @@ function [ph, info] = gcd_nls_complex(p, w, d, opt)
     end    
     opt.hini = lsdivmult(p, d, opt.gini);
   end    
-  opt.psi = [eye(2 * d + 2) kron(eye(d+1), [0 1; -1 0])];
-  opt.Rini = [real(opt.hini(:)') imag(opt.hini(:)')] * opt.psi;
+  
+  A = kron(eye(d+1), [1 0]);
+  B = kron(eye(d+1), [0 1]);
+  
+  opt.psi = [A B; -B A];
+  opt.Rini = [real(opt.hini(:).') imag(opt.hini(:).')] * opt.psi * sqrt(0.5);
   opt.Rini = reshape(opt.Rini, 2, 2*(d+1));
-  
-  disp(pext)
-  disp(bfp)
-  disp(opt.Rini)
-  
   
   s = struct('m', [d+1 d+1], 'n', bfn-d+1);
   s.gcd = 1;
@@ -37,4 +36,7 @@ function [ph, info] = gcd_nls_complex(p, w, d, opt)
       [ph, info] = slra(bfw.*bfp, s, sum(s.m)-2, opt);
     end 
   ph = mat2cell(ph, 2*(bfn+1), [1]);
+  split_complex = @(x) x(1:length(x)/2) + 1i * x(length(x)/2+1:end) ;
+  ph = cellfun(split_complex, ph, 'UniformOutput', false);
+  info.hh = split_complex(info.Rh(1,:).');
 end
